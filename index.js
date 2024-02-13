5443e4905f51889ec647c3d26b37f585b6445899
Nimra-create <nimraraza985@gmail.com>
2024-02-13 09:58:53 -0800

    PRACTICE

---
+++ index.js
@@ -0,0 +1,88 @@
+// var a = 2;   simple program
+ //var b = 3;
+// var c = 4;
+ //var sum = a + b + c;
+// console.log(sum)
+
+ //const num1 = parseInt(prompt('Enter the first number:'));
+ //const num2 = parseInt(prompt('Enter the second number:'));
+ //const sum = num1 + num2;
+ //console.log(`${sum}`);
+
+
+
+
+ //const number = prompt("Enter a number:");
+ //const num = parseInt(number);
+ // let sum = 0;
+// for (let digit of number) {
+//     sum += parseInt(digit);
+// }
+// console.log(`The sum of digits of ${number} is ${sum}.`);
+
+
+
+// const start = parseInt(prompt("Enter the start of the range:"));
+// const end = parseInt(prompt("Enter the end of the range:"));
+// let sum = 0;
+// for (let i = start; i <= end; i++) {
+//     sum += i;
+// }
+// console.log(`The sum of the numbers from ${start} to ${end} is ${sum}.`);
+
+
+
+
+
+// const numbers = [10, 20, 30, 40, 50];
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+// }
+
+// const average = sum / numbers.length;
+
+// console.log(`The sum of the numbers is: ${sum}`);
+// console.log(`The average of the numbers is: ${average}`);
+
+
+
+// const string1 = "hello";
+// const string2 = "world";
+
+// const length1 = string1.length;
+// const length2 = string2.length;
+
+// if (length1 < length2) {
+//     console.log(`${string1} has fewer characters than ${string2}`);
+// } else if (length1 > length2) {
+//     console.log(`${string1} has more characters than ${string2}`);
+// } else {
+//     console.log(`${string1} has the same number of characters as ${string2}`);
+//}
+
+
+
+// let str = "hello";
+
+// // Check if the string has at least two characters
+// if (str.length >= 2) {
+//     // Extract the first and last characters
+//     let firstChar = str[0];
+//     let lastChar = str[str.length - 1];
+
+//     // Construct the new string by swapping the first and last characters
+//     str = lastChar + str.slice(1, -1) + firstChar;
+
+//     console.log("After swapping:", str);
+// } else {
+//     console.log("String has less than two characters, cannot swap.");
+// }
+
+
+
+
+
+
+ 
